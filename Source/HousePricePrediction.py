ef426c9162cc3bb080c368d7aa6f182fcbf9cfaa
Bhavya Teja <bhavyateja7@gmail.com>
2017-12-05 17:45:41 -0600

    Visualized the Sale price of the train data

--- Source/HousePricePrediction.py
+++ Source/HousePricePrediction.py
@@ -9,7 +9,9 @@ Created on Tue Dec  5 16:32:57 2017
 #Importing the packages
 
 import pandas as pd
+import numpy as np
 import sklearn as sk
+import matplotlib.pyplot as plt
 from pandas.api.types import is_string_dtype
 from pandas.api.types import is_numeric_dtype
 
@@ -19,7 +21,25 @@ from pandas.api.types import is_numeric_dtype
 Train_data = pd.read_csv('/Users/bhavyateja/Github_Projects/House-Price-Prediction/Dataset/train.csv')
 Test_data = pd.read_csv('/Users/bhavyateja/Github_Projects/House-Price-Prediction/Dataset/test.csv')
 
+#Summary of the datasets
+
+Train_data.describe()
+
 #Listing the columns with character data types in both test and training datasets
 
 Character_columns_Train = [col for col in Train_data.columns if is_string_dtype(Train_data[col]) == True]
 Character_columns_Test = [col for col in Train_data.columns if is_numeric_dtype(Train_data[col]) == True]
+
+#Adding the column Sale price to the Test dataset
+
+Test_data['SalePrice'] = pd.Series(np.NaN, index = Test_data.index)
+
+#Analyzing the Sale Price column in Train Data and visualizing it via Histogram
+
+Train_data['SalePrice'].describe()
+
+plt.hist(Train_data['SalePrice'])
+plt.xlabel('Sale price of the house')
+plt.show()
+
+#Combining both the Test and Train data
